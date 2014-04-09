c30d6e7b07473364f5abc52d20f1d1c8eb23e82b
kushagrasharma <ksharm01@gmail.com>
2014-04-08 20:25:03 -0700

    Initial commit

---
+++ TouchTracker/BNRDrawView.m
@@ -0,0 +1,253 @@
+//
+//  BNRDrawView.m
+//  TouchTracker
+//
+//  Created by John Gallagher on 1/9/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRDrawView.h"
+#import "BNRLine.h"
+
+@interface BNRDrawView () <UIGestureRecognizerDelegate>
+
+@property (nonatomic, strong) NSMutableDictionary *linesInProgress;
+@property (nonatomic, strong) NSMutableArray *finishedLines;
+@property (nonatomic, weak) BNRLine* selectedLine;
+@property (nonatomic, strong)UIPanGestureRecognizer *moveRecognizer;
+@property (nonatomic) int lineWidth;
+
+@end
+
+@implementation BNRDrawView
+#pragma mark - Init
+- (instancetype)initWithFrame:(CGRect)r
+{
+    self = [super initWithFrame:r];
+
+    if (self) {
+        self.linesInProgress = [[NSMutableDictionary alloc] init];
+        self.finishedLines = [[NSMutableArray alloc] init];
+        self.backgroundColor = [UIColor grayColor];
+        self.multipleTouchEnabled = YES;
+        UITapGestureRecognizer *doubleTapRecognizer = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(doubleTap:)];
+        doubleTapRecognizer.numberOfTapsRequired = 2;
+        doubleTapRecognizer.delaysTouchesBegan = YES;
+        [self addGestureRecognizer:doubleTapRecognizer];
+        UITapGestureRecognizer *tgr = [[UITapGestureRecognizer alloc]initWithTarget:self action:@selector(tap:)];
+        tgr.delaysTouchesBegan = YES;
+        [tgr requireGestureRecognizerToFail:doubleTapRecognizer];
+        [self addGestureRecognizer:tgr];
+        UILongPressGestureRecognizer* longPress = [[UILongPressGestureRecognizer alloc]initWithTarget:self action:@selector(longPress:)];
+        [self addGestureRecognizer:longPress];
+        self.moveRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(moveLine:)];
+        self.moveRecognizer.delegate = self;
+        self.moveRecognizer.cancelsTouchesInView = NO;
+        [self addGestureRecognizer:self.moveRecognizer];
+    }
+
+    return self;
+}
+#pragma mark - Drawing methods
+- (void)strokeLine:(BNRLine *)line
+{
+    UIBezierPath *bp = [UIBezierPath bezierPath];
+    bp.lineWidth = 10;
+    bp.lineCapStyle = kCGLineCapRound;
+
+    [bp moveToPoint:line.begin];
+    [bp addLineToPoint:line.end];
+    [bp stroke];
+}
+
+- (void)drawRect:(CGRect)rect
+{
+    // Draw finished lines in black
+    [[UIColor blackColor]set];
+    for (BNRLine *line in self.finishedLines) {
+        //CGFloat angle = [self pointPairToBearingDegrees:line.begin secondPoint:line.end];
+        //int intpart = (int)angle;
+        //double decpart = angle - intpart;
+        //[[UIColor colorWithHue:decpart saturation:1 brightness:1 alpha:1]set];
+        [self strokeLine:line];
+    }
+
+    [[UIColor redColor] set];
+    for (NSValue *key in self.linesInProgress) {
+        [self strokeLine:self.linesInProgress[key]];
+    }
+    if (self.selectedLine) {
+        [[UIColor greenColor]set];
+        [self strokeLine:self.selectedLine];
+    }
+}
+-(BNRLine*)lineAtPoint:(CGPoint)p{
+    for (BNRLine* l in self.finishedLines) {
+        CGPoint start = l.begin;
+        CGPoint end = l.end;
+        for (float t = 0.0; t <= 1.0 ; t += 0.05) {
+            float x = start.x + t * (end.x - start.x);
+            float y = start.y + t * (end.y - start.y);
+            if (hypot(x - p.x, y - p.y) < 20.0) {
+                return l;
+            }
+        }
+    }
+    return nil;
+}
+#pragma mark - Touch Handlers
+-(void)tap:(UIGestureRecognizer*)gr{
+    NSLog(@"Tap");
+    CGPoint p = [gr locationInView:self];
+    self.selectedLine = [self lineAtPoint:p];
+    
+    if (self.selectedLine) {
+        [self becomeFirstResponder];
+        UIMenuController *menu = [UIMenuController sharedMenuController];
+        UIMenuItem *deleteItem = [[UIMenuItem alloc]initWithTitle:@"Delete" action:@selector(deleteLine:)];
+        menu.menuItems = @[deleteItem];
+        [menu setTargetRect:CGRectMake(p.x, p.y, 2, 2) inView:self];
+        [menu setMenuVisible:YES animated:YES];
+    }
+    else{
+        [[UIMenuController sharedMenuController]setMenuVisible:NO animated:YES];
+    }
+    [self setNeedsDisplay];
+}
+-(void)doubleTap:(UIGestureRecognizer*)gr{
+    NSLog(@"Double tap");
+    [self.linesInProgress removeAllObjects];
+    [self.finishedLines removeAllObjects];
+    [self setNeedsDisplay];
+    
+}
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer{
+    if (gestureRecognizer == self.moveRecognizer) {
+        return YES;
+    }
+    return NO;
+}
+-(BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRequireFailureOfGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer{
+    if (gestureRecognizer == self.moveRecognizer && [otherGestureRecognizer isKindOfClass:[UITapGestureRecognizer class]]) {
+        return YES;
+    }
+    return NO;
+}
+-(void)moveLine:(UIPanGestureRecognizer*)gr{
+    if (!self.selectedLine) {
+        return;
+    }
+    if (gr.state == UIGestureRecognizerStateChanged) {
+        CGPoint translation = [gr translationInView:self];
+        CGPoint begin = self.selectedLine.begin;
+        CGPoint end = self.selectedLine.end;
+        begin.x += translation.x;
+        begin.y += translation.y;
+        end.y += translation.y;
+        end.x += translation.x;
+        self.selectedLine.begin = begin;
+        self.selectedLine.end = end;
+        [gr setTranslation:CGPointZero inView:self];
+        [self setNeedsDisplay];
+    }
+}
+- (void)touchesBegan:(NSSet *)touches
+           withEvent:(UIEvent *)event
+{
+    if (self.selectedLine) {
+        self.selectedLine = nil;
+        [[UIMenuController sharedMenuController]setMenuVisible:NO animated:YES];
+    }
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
+    for (UITouch *t in touches) {
+        CGPoint location = [t locationInView:self];
+
+        BNRLine *line = [[BNRLine alloc] init];
+        line.begin = location;
+        line.end = location;
+
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        self.linesInProgress[key] = line;
+    }
+    
+    [self setNeedsDisplay];
+}
+
+- (void)touchesMoved:(NSSet *)touches
+           withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        BNRLine *line = self.linesInProgress[key];
+
+        line.end = [t locationInView:self];
+    }
+
+    [self setNeedsDisplay];
+}
+
+- (void)touchesEnded:(NSSet *)touches
+           withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        BNRLine *line = self.linesInProgress[key];
+
+        [self.finishedLines addObject:line];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+
+    [self setNeedsDisplay];
+}
+
+- (void)touchesCancelled:(NSSet *)touches
+               withEvent:(UIEvent *)event
+{
+    // Let's put in a log statement to see the order of events
+    NSLog(@"%@", NSStringFromSelector(_cmd));
+
+    for (UITouch *t in touches) {
+        NSValue *key = [NSValue valueWithNonretainedObject:t];
+        [self.linesInProgress removeObjectForKey:key];
+    }
+
+    [self setNeedsDisplay];
+}
+-(void)longPress:(UIGestureRecognizer*)gr{
+    if (gr.state == UIGestureRecognizerStateBegan) {
+        CGPoint p = [gr locationInView:self];
+        self.selectedLine = [self lineAtPoint:p];
+        if (self.selectedLine) {
+            [self.linesInProgress removeAllObjects];
+        }
+    }
+    else if (gr.state == UIGestureRecognizerStateEnded){
+        self.selectedLine = nil;
+    }
+    [self setNeedsDisplay];
+}
+#pragma mark - Others
+- (CGFloat) pointPairToBearingDegrees:(CGPoint)startingPoint secondPoint:(CGPoint) endingPoint
+{
+    CGPoint originPoint = CGPointMake(endingPoint.x - startingPoint.x, endingPoint.y - startingPoint.y); // get origin point to origin by subtracting end from start
+    float bearingRadians = atan2f(originPoint.y, originPoint.x); // get bearing in radians
+    float bearingDegrees = bearingRadians * (180.0 / M_PI); // convert to degrees
+    bearingDegrees = (bearingDegrees > 0.0 ? bearingDegrees : (360.0 + bearingDegrees)); // correct discontinuity
+    return bearingDegrees;
+}
+-(BOOL)canBecomeFirstResponder{
+    return YES;
+}
+-(void)deleteLine:(id)sender{
+    [self.finishedLines removeObject:self.selectedLine];
+    [self setNeedsDisplay];
+}
+@end
